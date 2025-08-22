8071ba72d15df079f65d5764e32e50bd9d1805ae
FabrizioNegron <fabrizio.negron.quispe@vallegrande.edu.pe>
2025-08-22 17:13:39 +0000

    primer commit del proyecto

---
+++ src/main/java/pe/edu/vallegrande/fabrizio/FabrizioApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.vallegrande.fabrizio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabrizioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabrizioApplication.class, args);
+	}
+
+}
